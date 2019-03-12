a0d37f8a24af561eb384cfc7b145a088213baa3f
AlfieRrc0032574 <rc0032574@student.ac.uk>
2019-03-12 10:50:19 +0000

    Add project files.

---
+++ forces 2.0/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace forces_2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        double sin(double x)
+        {
+            return (Math.Sin(x * Math.PI / 180.0));
+        }
+
+        double cos(double x)
+        {
+            return (Math.Cos(x * Math.PI / 180.0));
+        }
+
+        double asin(double x)
+        {
+            return (asin(x) * 180 / Math.PI);
+        }
+
+        double acos(double x)
+        {
+            return (acos(x) * 180 / Math.PI);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double force = double.Parse(textBox2.Text);
+            double angle = double.Parse(textBox1.Text);
+
+            double Fx = force * cos(angle);
+            double Fy = angle * sin(angle);
+
+            MessageBox.Show("Fx = " + Fx + " N");
+            MessageBox.Show("Fy = " + Fy + " N");
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
